b8e5a35c445b15e5d82b2ecbcd9c4c284b151ddc
William Melody <wmelody@gmail.com>
2008-11-01 18:24:57 -0500

    allowing active_nav_tab to act as a before filter

--- lib/active_nav_tabs.rb
+++ lib/active_nav_tabs.rb
@@ -2,20 +2,24 @@
 module ActiveNavTabs
   module ClassMethods
     
-    # Use ad a before_filter to define the the tab list.
+    # Use as a before_filter to define the the tab list.
     # Options:
     #   :nav_tabs => takes an array in this format:
     #     [{:id => 'id', :path => '/path', :text => 'Text'}, ...]
     #     * if :path is ommitted, it is derived from the id by adding a leading '/'
     #     * if :text is ommitted, the id is capitalized and used
     #   :active_tab_background => takes a hex color value
-    def active_nav_tabs(options = {}, &block)
-      block ||= lambda do |c|
-          c.nav_tabs              = options[:nav_tabs] || options[:tabs]
-          c.active_tab_background = options[:active_tab_background]
-          c.reset_to_horizontal   = options[:reset_to_horizontal]
+    def active_nav_tabs(args, &block)
+      if args.is_a?(Symbol)
+        before_filter(args, &block)
+      else
+        block ||= lambda do |c|
+            c.nav_tabs              = args[:nav_tabs] || args[:tabs]
+            c.active_tab_background = args[:active_tab_background]
+            c.reset_to_horizontal   = args[:reset_to_horizontal]
+        end
+        before_filter(block)
       end
-      before_filter(block, options)
     end
     
   end
